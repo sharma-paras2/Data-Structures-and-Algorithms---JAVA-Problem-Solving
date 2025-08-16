51a6992bfeb0e1d318a5405d38ffe99a6d5db5cf
Parassharma1210 <152158874+Parassharma1210@users.noreply.github.com>
2025-08-16 13:21:18 +0530

    checking git

---
+++ SwapArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SwapArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr= new int[5];
+        System.out.print("enter the elements of array");
+        for(int i = 0;i<5;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int i,j;
+        i=0;
+        j=4;
+       while (i<j) {
+              int temp =arr[i];
+              arr[i]=arr[j];
+              arr[j]= temp;
+              i++;
+              j--;
+       }
+       System.out.println("the new array will be");
+        for(int k = 0;k<5;k++)
+        {
+            System.out.print(" "+arr[k]);
+        }
+        sc.close();
+    }
+
+    
+}
